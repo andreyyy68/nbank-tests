0df7fc1cd1538e9103ba6f0464207649ee4af7ca
Andrew <andrew@yandex.ru>
2025-10-31 21:02:52 +0300

    New Debug

--- run-tests.sh
+++ run-tests.sh
@@ -2,25 +2,20 @@
 
 # Настройка
 IMAGE_NAME=nbank-test
-TEST_PROFILE=${1:-api}
-TIMESTAMP=$(date +"%Y%m%d_%H%M")
-TEST_OUTPUT_DIR="$(pwd)/test-output/$TIMESTAMP"
 
-# Собираем докер образ
-echo ">>> Сборка тестов запущена"
+
 docker build -t $IMAGE_NAME .
 
 mkdir -p "$TEST_OUTPUT_DIR/logs"
 mkdir -p "$TEST_OUTPUT_DIR/reports"
 
-echo ">>> Тесты запущены"
 
 docker run --rm \
+  --network nbank-network \
   -v "$TEST_OUTPUT_DIR/logs":/app/logs \
   -v "$TEST_OUTPUT_DIR/reports":/app/reports \
   -e TEST_PROFILE="$TEST_PROFILE" \
-  -e BACKEND_URL="http://host.docker.internal:4111/api/v1" \
-  -e UI_BASE_URL="http://host.docker.internal:3000" \
+  -e BACKEND_URL="http://backend:4111/api/v1" \
+  -e UI_BASE_URL="http://frontend" \
   $IMAGE_NAME
 
-echo ">>> Тесты завершены"
