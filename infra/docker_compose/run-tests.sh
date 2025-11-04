1a33860f8b8fa2b8861b8d01f623a87840a6bd60
Andrew <andrew@yandex.ru>
2025-11-04 18:25:07 +0300

    Debug pipe

--- infra/docker_compose/run-tests.sh
+++ infra/docker_compose/run-tests.sh
@@ -1,29 +1,22 @@
 #! /bin/bash
 
 IMAGE_NAME=nbank-test
-TEST_PROFILE=${1:-"api"}  # api или ui
-TIMESTAMP=$(date +"%Y%m%d_%H%M%S")
-TEST_OUTPUT_DIR="$(pwd)/test-output/$TIMESTAMP"
+TEST_PROFILE=${1:-"api"}
+TEST_OUTPUT_DIR="$(pwd)/test-output"
 RAW_REPORTS_DIR="$TEST_OUTPUT_DIR/reports/raw"
 ALLURE_HTML_DIR="$TEST_OUTPUT_DIR/reports/html"
 LOGS_DIR="$TEST_OUTPUT_DIR/logs"
 
-# ==========================
-# Создаём папки для результатов
-# ==========================
+
+
 mkdir -p "$RAW_REPORTS_DIR"
 mkdir -p "$ALLURE_HTML_DIR"
 mkdir -p "$LOGS_DIR"
 
-# ==========================
-# Сборка Docker образа
-# ==========================
 echo ">>> Сборка Docker образа: $IMAGE_NAME"
 docker build -t $IMAGE_NAME .
 
-# ==========================
-# Запуск тестов в Docker с генерацией raw Allure результатов
-# ==========================
+
 echo ">>> Запуск тестов профиля: $TEST_PROFILE"
 
 docker run --rm \
