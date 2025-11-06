7792ec7d971bda7dbeaf601dcfba91162638cca8
Andrew <andrew@yandex.ru>
2025-11-06 19:19:31 +0300

    New Debug 000

--- src/main/ui/conftest.py
+++ src/main/ui/conftest.py
@@ -4,6 +4,8 @@ from src.main.api.models.create_user_request import CreateUserRequest
 from src.main.ui.helpers.context import add_item_to_local_storage
 from src.main.classes.api_manager import ApiManager
 from src.main.configs.config import Config
+import allure
+import time
 
 
 @pytest.fixture(scope="session")
@@ -23,6 +25,22 @@ def browser(playwright: Playwright):
 def new_context(browser: Browser, browser_context_args):
     context = browser.new_context(**browser_context_args, record_video_dir="test-output/videos")
     yield context
+    for idx, page in enumerate(context.pages, start=1):
+        try:
+            page.close()
+        except Exception:
+            pass
+        try:
+            video_path = page.video.path()
+            timestamp = int(time.time() * 1000)
+            allure.attach.file(
+                video_path,
+                name=f"video_{idx}_{timestamp}.webm",
+                attachment_type=allure.attachment_type.WEBM
+            )
+        except Exception:
+            pass
+
     context.close()
 
 @pytest.fixture
@@ -48,7 +66,6 @@ def admin_session(new_context, admin_token):
         item_value=admin_token,
     )
     yield new_context
-    new_context.close()
 
 
 @pytest.fixture
@@ -59,7 +76,6 @@ def user_session(new_context, user_token):
         item_value=user_token,
     )
     yield new_context
-    new_context.close()
 
 
 @pytest.fixture
--- src/main/ui/tests/deposit_user_ui_test.py
+++ src/main/ui/tests/deposit_user_ui_test.py
@@ -6,6 +6,7 @@ import pytest
 
 
 
+
 @pytest.mark.regression
 @pytest.mark.ui
 class TestDepositUser:
