6a5396dff4af3b51758786c12c121eedf8df335b
Andrew <andrew@yandex.ru>
2025-11-07 11:49:53 +0300

    101 01

--- src/main/ui/pages/transfer_page.py
+++ src/main/ui/pages/transfer_page.py
@@ -20,42 +20,26 @@ class TransferPage(BasePage):
     def url(self):
         return "/transfer"
 
-    def select_account(self, account_id, amount):
-        option_selector = f"option[value='{account_id}']"
-        for attempt in range(1, 5):
-            if attempt > 1:
-                self.page.reload(wait_until="networkidle")
-            try:
-                option = self.choose_an_account.locator(option_selector)
-                text = option.text_content()
-                match = re.search(r"Balance: \$([\d.]+)", text)
-                if not match:
-                    raise ValueError(f"Не удалось извлечь баланс из {text}")
-
-                balance = float(match.group(1))
-                if balance >= amount:
-                  self.choose_an_account.select_option(value=str(account_id))
-                  self.take_screenshot("choose_an_account")
-                else:
-                    self.page.reload()
-                    self.choose_an_account.wait_for(state="attached")
-            except AssertionError:
-                raise "Баланс"
-
+    def select_account(self, account_id):
+        self.choose_an_account.select_account(value=str(account_id))
         return self
 
     def enter_recipient_name(self, name):
-        self.recipient_name.fill(name)
+        self.recipient_name.click()
+        self.recipient_name.type(name, delay=50)
+        expect(self.transfer_button).to_be_enabled(timeout=10000)
         self.take_screenshot("recipient_name")
         return self
 
     def enter_recipient_account_number(self, account_number):
-        self.recipient_account_number.fill(account_number)
+        self.recipient_account_number.click()
+        self.recipient_account_number.type(account_number, delay=50)
         self.take_screenshot("recipient_account_number")
         return self
 
     def enter_amount(self, amount):
-        self.fill_amount.fill(str(amount))
+        self.fill_amount.click()
+        self.fill_amount.type(str(amount))
         self.take_screenshot("fill_amount")
         return self
 
